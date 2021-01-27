d238363373fa7947d0e3ff740c069f20c165d403
Antonio Soto <fantoniosoto@gmail.com>
2021-01-27 14:24:25 -0500

    changes on user model

--- db/models/user.js
+++ db/models/user.js
@@ -34,14 +34,23 @@ const UserSchema = new Schema({
       type: String
     },
     birthdate: Date,
-    insurance_type: String,
+    insurance_type: {
+      type: String,
+      enum: ['rimac eps/seguros', 'mapfre eps/seguros', 'pacifico eps/seguros', 'sanitas peru/eps', 'la positiva eps/seguros', 'la protectora', 'semefa', 'otros']
+    },
     location: {
       district: String,
       province: String,
       department: String
     },
-    medical_history: [String],
-    drug_allergy: [String],
+    medical_history: [{
+      type: String,
+      enum: ['hipertension arterial', 'diabetes mellitus', 'cancer', 'enfermedad de la tiroides', 'infarto miocardio', 'acv (derrame)-isquemia', 'enfermedad hepatica', 'enfermedad renal', 'artritis-artrosis', 'glaucoma o enfermedad en ojos', 'obesidad', 'exposicion a ruido laboral', 'epilepsia', 'vasculitis o enfermedad reumatologica', 'condicion mental']
+    }],
+    drug_allergy: [{
+      type: String,
+      enum: ['penicilina', 'aines', 'sulfametoxazol', 'yodo', 'ciprofloxacino', 'corticoides', 'tramadol', 'benzodiazepinas', 'azitromicina', 'paracetamol', 'metamizol']
+    }],
     appointments: [{
       type: Schema.Types.ObjectId,
       ref: 'appointment'
@@ -71,11 +80,11 @@ const UserSchema = new Schema({
     },
     experience: {
       type: String,
-      enum: ['Menor de 5 años', 'Mayor de 5 años']
+      enum: ['menor de 5 años', 'entre 5 a 15 años', 'entre 15 a 30 años', 'mayor de 30 años']
     },
     specialty: {
       type: String,
-      enum: ['adolescentologia', 'cirugia']
+      enum: ['adolescentologia', 'alergia e inmunologia/infantil', 'cardiologia/infantil', 'cirugia cardiovascular/pediatrica', 'cirugia cabeza, cuello y maxilofacial', 'cirugia de mano', 'cirugia de rodilla', 'cirugia de torax y cardiovascular', 'cirugia general', 'cirugia oncologica abdominal', 'cirugia oncologica cabeza y cuello', 'cirugia oncologica de mamas', 'cirugia ortopedia y traumatologia', 'cirugia pediatrica', 'cirugia plastica y facial', 'cirugia plastica y reconstructiva', 'dermatologia', 'dermatologia pediatrica', 'endocrinologia', 'endocrinologia pediatrica', 'enfermedades infecciosas/infectologia', 'flebologia', 'gastroenterologia', 'gastroenterologia pediatrica', 'geriatria', 'ginecologia y obstetricia', 'ginecologia oncologica', 'hematologia/pediatrica', 'medicina familiar y comunitaria', 'medicina interna', 'medicina integrativa y ortomolecular', 'medicina fisica y rehabilitacion', 'nefrologia/pediatrica', 'neonatologia', 'neumologia/pediatrica', 'neurocirugia/pediatrica', 'neurologia/pediatrica', 'nutricion', 'oftalmologia/pediatrica', 'oncologia/pediatrica', 'oncologia radioterapia', 'ortopedia y traumatologia', 'otorrinolaringologia/pediatrica', 'pediatria/puericultura', 'psiquiatria/infantil y adolescentes', 'psiquiatria de adicciones', 'psicologia', 'reumatologia', 'urologia/pediatrica', 'urologia oncologica']
     },
     payment_method: {
       type: String,
